ab3e4cbaceadcfadeab08d9cd70dfe2069bf361d
okitavios1801@gmail.com <Otvio@DESKTOP-MTKKSDG>
2025-09-16 13:01:32 -0300

    Atualização da classe Node

--- TrabalhoED/src/Trabalho/Node.java
+++ TrabalhoED/src/Trabalho/Node.java
@@ -8,5 +8,22 @@ public class Node {
 	this.next=null;
 	
 	}
+	public Processo getProcesso() {
+		return p;}
+	public void setProcesso(Processo p) {
+		this.p=p;
+	}
+	//
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next=next;
+	}
+	
+	@Override
+	public String toString() {
+        return p == null ? "null" : p.toString();
+    }
 	
 }
